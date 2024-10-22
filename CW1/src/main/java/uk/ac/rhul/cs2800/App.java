a9972d269f57757d4a8a6f190c2fbc73a5da1fe0
Jared Tan <Jared.Tan.2023@live.rhul.ac.uk>
2024-10-22 20:19:25 +0100

    fixed App.Java

--- CW1/src/main/java/uk/ac/rhul/cs2800/App.java
+++ CW1/src/main/java/uk/ac/rhul/cs2800/App.java
@@ -1,10 +1,10 @@
 package uk.ac.rhul.cs2800;
 
 /**
- * Hello world!
+ * Hello world!.
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
