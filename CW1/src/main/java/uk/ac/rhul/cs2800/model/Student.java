c29a97f8ebcb41a9df8c77dae8dcf13134db6e7b
Jared Tan <Jared.Tan.2023@live.rhul.ac.uk>
2024-10-30 13:47:18 +0000

    Added 'else' for 'if-else'

--- CW1/src/main/java/uk/ac/rhul/cs2800/model/Student.java
+++ CW1/src/main/java/uk/ac/rhul/cs2800/model/Student.java
@@ -78,6 +78,8 @@ public class Student {
         } else {
           throw new NoGradeAvailableException();
         }
+      } else {
+        continue;
       }
     }
     throw new NoRegistrationException();
