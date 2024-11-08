c1c39c999b171d8b981bdf206f1c1777c8150d9a
Jared Tan <Jared.Tan.2023@live.rhul.ac.uk>
2024-11-08 19:57:38 +0000

    Modified application.properties and added classes from CW1 #3

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/model/Registration.java
@@ -0,0 +1,40 @@
+package uk.ac.rhul.cs2800.model;
+
+/** This is for the student to register. */
+public class Registration extends Student {
+  private Module module;
+
+  /**
+   * Constructors.
+   *
+   * @param id The primary key of student.
+   * @param firstName The first half of the student's name.
+   * @param lastName The last half of the student's name.
+   * @param userName The identifying username.
+   * @param email The student's email.
+   * @param module The student is registering.
+   */
+  public Registration(long id, String firstName, String lastName, String userName, String email,
+      Module module) {
+    super(id, firstName, lastName, userName, email);
+    this.module = module;
+  }
+
+  /**
+   * Get the module and retunr the module.
+   *
+   * @return the module.
+   */
+  public Module getModule() {
+    return module;
+  }
+
+  /**
+   * Set the module in registration.
+   *
+   * @param module the module to set.
+   */
+  public void setModule(Module module) {
+    this.module = module;
+  }
+}
---
+++ CW2/src/test/java/uk/ac/rhul/cs2800/model/AppTest.java
@@ -0,0 +1,148 @@
+package uk.ac.rhul.cs2800.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.ac.rhul.cs2800.exception.NoGradeAvailableException;
+import uk.ac.rhul.cs2800.exception.NoRegistrationException;
+
+/** Unit tests. Testing methods and exceptions. */
+public class AppTest {
+
+  Student student;
+
+  @BeforeEach
+  void beforeEach() {
+    student = new Student(0, null, null, null, null);
+  }
+
+  /**
+   * Gets the average of all the Grade scores the student has (in a certain module).
+   *
+   * @throws NoGradeAvailableException If no grade is available/grade doesn't exist.
+   */
+  @Test
+  void computeAverageTest() throws NoGradeAvailableException {
+    for (int i = 0; i < 3; i++) {
+      student.addGrade(new Grade(null, null, false, i + 1));
+    }
+    assertNotNull(student.grades.get(0).getScore()); // Test 1
+    assertNotNull(student.grades.get(1).getScore()); // Test 2
+    assertNotNull(student.grades.get(2).getScore()); // Test 3
+    assertNotNull(student.grades.size()); // Test 4
+    assertEquals(1, student.grades.get(0).getScore()); // Test 5
+    assertEquals(2, student.grades.get(1).getScore()); // Test 6
+    assertEquals(3, student.grades.get(2).getScore()); // Test 7
+    assertEquals(3, student.grades.size()); // Test 8
+    assertEquals(2, student.computeAverage()); // Test 9
+  }
+
+  /**
+   * Check if grades are actually added.
+   *
+   * @throws NoGradeAvailableException.
+   */
+  @Test
+  void addGradeTest() throws NoGradeAvailableException {
+    student.addGrade(new Grade(null, null, false, 10));
+    student.addGrade(new Grade(null, null, false, 99));
+    assertNotNull(student.grades.get(0).getScore()); // Test 10
+    assertNotNull(student.grades.get(1).getScore()); // Test 11
+    assertEquals(10, student.grades.get(0).getScore()); // Test 12
+    assertEquals(99, student.grades.get(1).getScore()); // Test 13
+  }
+
+  /**
+   * Check if you actually get the grade of the student you assigned it to.
+   *
+   * @throws NoGradeAvailableException If no grade is available/grade doesn't exist.
+   * @throws NoRegistrationException If a user try to access grades for unregistered modules.
+   */
+  @Test
+  void getGradeTest() throws NoGradeAvailableException, NoRegistrationException {
+    Grade grade1 = new Grade(null, null, false, 10);
+    Grade grade2 = new Grade(null, null, false, 5); 
+    Module module1 = new Module(null, "module1", false, grade1); 
+    Module module2 = new Module(null, "module2", false, grade2); 
+    student.registerModule(module1);
+    student.registerModule(module2);
+    assertNotNull(student.registrations); // Test 14
+    assertNotNull(student.registrations.get(0).getModule()); // Test 15
+    assertEquals("module1", student.registrations.get(0).getModule().getName()); // Test 16
+    assertEquals("module2", student.registrations.get(1).getModule().getName()); // Test 17
+    assertEquals(10, student.getGrade(module1).getScore()); // Test 18
+    assertEquals(5, student.getGrade(module2).getScore()); // Test 19
+  }
+
+  /**
+   * Check if you actually get to register the student to the module. TODO Make the Test Work Then
+   * Work On Method.
+   *
+   * @throws NoGradeAvailableException If no grade is available/grade doesn't exist.
+   */
+  @Test
+  void getRegistrationTest() throws NoRegistrationException {
+    Module module = new Module(null, "Maths", false, null);
+    student.registerModule(module);
+    assertEquals("Maths", module.getName()); // Test 20
+    assertEquals(student.registrations.get(0).getModule().getName(), module.getName()); // Test 21
+  }
+
+  /**
+   * Check if the exception is working as intended. Relates to: If no grade is available/grade
+   * doesn't exist.
+   */
+  @Test
+  void NoGradeAvailableException() {
+    assertThrows(NoGradeAvailableException.class, () -> { // Test 22
+      student.computeAverage();
+    });
+    assertThrows(NoGradeAvailableException.class, () -> { // Test 23
+      Module module = new Module(null, null, false, null);
+      student.registerModule(module);
+      student.getGrade(module);
+    });
+  }
+
+  /**
+   * Check if the exception is working as intended. Relates to: If a user try to access grades for
+   * unregistered modules.
+   */
+  @Test
+  void NoRegistrationException() {
+    assertThrows(NoRegistrationException.class, () -> { // Test 24
+      Module module = new Module(null, "Party", false, null);
+      student.getGrade(module);
+    });
+    assertThrows(NoRegistrationException.class, () -> { // Test 25
+      Module module1 = new Module(null, "module1", false, null);
+      Module module2 = new Module(null, "module2", false, null);
+      student.registerModule(module1);
+      student.getGrade(module2);
+    });
+  }
+
+  /**
+   * Check if you can set and get a score for the grade
+   */
+  @Test
+  void gradeScoreTest() {
+    Grade grade = new Grade(null, null, false, 0);
+    grade.setScore(5);
+    assertEquals(5, grade.getScore()); // Test 26
+  }
+
+  /**
+   * Check if you can set and get a module for a registration
+   */
+  @Test
+  void registrationModuleTest() {
+    Registration registration = new Registration(0, null, null, null, null, null);
+    Module module = new Module(null, "module", false, null);
+    registration.setModule(module);
+    assertEquals("module", registration.getModule().getName()); // Test 27
+    assertEquals(module.getName(), registration.getModule().getName()); // Test 28
+  }
+}
