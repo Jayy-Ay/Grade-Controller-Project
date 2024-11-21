4f19978f1388fdf0df0a8d50b65ac2f3648b198e
Jared Tan <Jared.Tan.2023@live.rhul.ac.uk>
2024-11-21 20:00:12 +0000

    Unavoidable initial generation + application properties #11

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/Cw2Application.java
@@ -0,0 +1,22 @@
+package uk.ac.rhul.cs2800;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main application.
+ * 
+ */
+@SpringBootApplication
+public class Cw2Application {
+
+  /**
+   * Runs springboot website.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Cw2Application.class, args);
+  }
+
+}
