699e774f7e866b8633b9b5a4b74a4fe558102aec
Jared Tan <Jared.Tan.2023@live.rhul.ac.uk>
2024-11-22 16:43:00 +0000

    Added correct application properties #14

--- CW2/src/main/java/uk/ac/rhul/cs2800/Cw2Application.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/Cw2Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cw2Application {
 
+  /**
+   * This is to initialize the springboot.
+   *
+   * @param args
+   */
 	public static void main(String[] args) {
 		SpringApplication.run(Cw2Application.class, args);
 	}
