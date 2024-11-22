f0a6cff87c5c41d6e3c5f784ac7acf45900e0ae1
Tan Jared (2023) WMIS166 <jared.tan.2023@live.rhul.ac.uk>
2024-11-22 17:01:08 +0000

    Added CW1 Classes Part2 #14

--- CW2/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
@@ -6,3 +6,54 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+
+public class Grade {
+  @Id
+  Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "student_id")
+  Student student;
+
+  @OneToOne
+  @JoinColumn(name = "module_code")
+  Module module;
+
+  Integer score; // 'int' not used because 'Integer' allows null value.
+
+  /**
+   * Constructors.
+   *
+   * @param score The score of the grade.
+   */
+  public Grade(Integer score) {
+    this.score = score;
+  }
+
+  /**
+   * Get the score of this grade.
+   *
+   * @return the score as in integer.
+   */
+  public Integer getScore() {
+    return score;
+  }
+
+  /**
+   * Set the score for this grade.
+   *
+   * @param score the integer score to set.
+   */
+  public void setScore(Integer score) {
+    this.score = score;
+  }
+
+  /**
+   * Set the module for this grade.
+   *
+   * @param module relating to the grade to set.
+   */
+  public void setModule(Module module) {
+    this.module = module;
+  }
+}
