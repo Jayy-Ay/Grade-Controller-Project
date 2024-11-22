43a11004f952ba70007ecc2c7f01f04b07255220
Tan Jared (2023) WMIS166 <jared.tan.2023@live.rhul.ac.uk>
2024-11-22 16:59:39 +0000

    Added CW1 Classes Part1 #14

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
@@ -0,0 +1,8 @@
+package uk.ac.rhul.cs2800.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
